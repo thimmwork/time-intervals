5d6807a9b83b34a3fd4f17cc45016137006d455f
marcel <git@thimmwork.net>
2019-01-26 13:29:49 +0100

    added LocalDateIntervalMap

---
+++ core/src/main/java/net/thimmwork/time/interval/LocalDateIntervalMap.kt
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2018 thimmwork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.thimmwork.time.interval
+
+import com.google.common.collect.BoundType
+import com.google.common.collect.Range
+import com.google.common.collect.TreeRangeMap
+import java.time.LocalDate
+
+class LocalDateIntervalMap<T> {
+
+    private val delegate = TreeRangeMap.create<LocalDate, T>()
+
+    operator fun get(key: LocalDate) = delegate[key]
+
+    fun isEmpty(): Boolean {
+        val empty = delegate.asMapOfRanges().isEmpty()
+        if (empty) return true
+
+        return delegate.asMapOfRanges().keys.all { it.isEmpty }
+    }
+
+    fun asMapOfLocalDateIntervals() : Map<LocalDateInterval, T> {
+        return delegate.asMapOfRanges()
+                .mapKeys { toLocalDateInterval(it.key) }
+    }
+
+    private fun toLocalDateInterval(range: Range<LocalDate>): LocalDateInterval {
+        val lowerEndpoint : LocalDate =
+                if (range.lowerBoundType() == BoundType.CLOSED) range.lowerEndpoint()
+                else range.lowerEndpoint().plusDays(1)
+        val upperEndpoint =
+                if (range.upperBoundType() == BoundType.CLOSED) range.upperEndpoint()
+                else range.upperEndpoint().minusDays(1)
+        return LocalDateInterval(lowerEndpoint, upperEndpoint)
+    }
+
+    fun clear() = delegate.clear()
+
+    fun put(key: LocalDateInterval, value: T) {
+        delegate.put(key.toRange(), value)
+    }
+
+    fun putAll(from: LocalDateIntervalMap<T>) {
+        from.delegate.asMapOfRanges().entries.forEach { delegate.put(it.key, it.value)}
+    }
+
+    fun remove(key: LocalDateInterval) {
+        val toRange = key.toRange()
+        val range = Range.open(toRange.lowerEndpoint().minusDays(1), toRange.upperEndpoint().plusDays(1))
+        delegate.remove(range)
+    }
+}
---
+++ core/src/test/java/net/thimmwork/time/interval/LocalDateIntervalMapTest.kt
@@ -0,0 +1,128 @@
+/**
+ * Copyright 2018 thimmwork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.thimmwork.time.interval
+
+import net.thimmwork.time.interval.LocalDateInterval.Companion.parse
+import org.junit.Test
+import java.time.LocalDate
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class LocalDateIntervalMapTest {
+    private val interval2018 = parse("2018-01-01", "2018-12-31")
+    private val firstHalf2018 = parse("2018-01-01", "2018-06-30")
+    private val secondHalf2018 = parse("2018-07-01", "2018-12-31")
+
+    @Test
+    fun `get returns a previously put value for both bounds and null for date beyond bound`() {
+        val map = LocalDateIntervalMap<Int>()
+        map.put(interval2018, 4711)
+
+        assertTrue { map[interval2018.start] == 4711 }
+        assertTrue { map[interval2018.end] == 4711 }
+
+        assertTrue { map[interval2018.start-1] == null }
+        assertTrue { map[interval2018.end+1] == null }
+    }
+
+    @Test
+    fun `when a map is created then it is empty`() {
+        val map = LocalDateIntervalMap<Int>()
+        assertTrue { map.isEmpty() }
+    }
+
+    @Test
+    fun `when an interval is put then the map is not empty - when the interval is removed then it is empty again`() {
+        val map = LocalDateIntervalMap<Int>()
+
+        map.put(interval2018, 4711)
+        assertFalse { map.isEmpty() }
+
+        map.remove(interval2018)
+        assertTrue { map.isEmpty() }
+    }
+
+    @Test
+    fun `when all partial intervals are removed then the map is empty`() {
+        val map = LocalDateIntervalMap<Int>()
+
+        map.put(interval2018, 4711)
+
+        map.remove(firstHalf2018)
+        assertFalse { map.isEmpty() }
+        map.remove(secondHalf2018)
+        assertTrue { map.isEmpty() }
+    }
+
+    @Test
+    fun asMapOfLocalDateIntervals() {
+        val map = LocalDateIntervalMap<Int>()
+        map.put(firstHalf2018, 4711)
+        map.put(secondHalf2018, 4712)
+
+        val asMapOfLocalDateIntervals = map.asMapOfLocalDateIntervals()
+
+        assertTrue { !asMapOfLocalDateIntervals.isEmpty() }
+        assertTrue { asMapOfLocalDateIntervals.size == 2  }
+        assertTrue { asMapOfLocalDateIntervals == mapOf(Pair(firstHalf2018, 4711), Pair(secondHalf2018, 4712))  }
+    }
+
+    @Test
+    fun clear() {
+        val map = LocalDateIntervalMap<Int>()
+        map.put(firstHalf2018, 4711)
+        map.put(secondHalf2018, 4712)
+
+        map.clear()
+
+        assertTrue { map.isEmpty() }
+        assertTrue { map.asMapOfLocalDateIntervals().isEmpty() }
+    }
+
+    @Test
+    fun putAll() {
+        val map = LocalDateIntervalMap<Int>()
+        val otherMap = LocalDateIntervalMap<Int>()
+        otherMap.put(firstHalf2018, 4711)
+        otherMap.put(secondHalf2018, 4712)
+
+        map.putAll(otherMap)
+        otherMap.remove(secondHalf2018)
+
+        assertFalse { otherMap.get(secondHalf2018.start) == 4712 }
+        assertTrue { map.get(secondHalf2018.start) == 4712 }
+    }
+
+    @Test
+    fun `put replaces existing values`() {
+        val map = LocalDateIntervalMap<Int>()
+        map.put(interval2018, 1)
+
+        map.put(firstHalf2018, 2)
+
+        val mapOfLocalDateIntervals = map.asMapOfLocalDateIntervals()
+        assertTrue { mapOfLocalDateIntervals[firstHalf2018] == 2}
+        assertTrue { map[firstHalf2018.start] == 2 }
+        assertTrue { map[firstHalf2018.end] == 2 }
+        assertTrue { mapOfLocalDateIntervals[secondHalf2018] == 1}
+        assertTrue { map[secondHalf2018.start] == 1}
+        assertTrue { map[secondHalf2018.end] == 1}
+    }
+}
+
+operator fun LocalDate.minus(daysToSubtract: Long) = this.minusDays(daysToSubtract)
+operator fun LocalDate.plus(daysToAdd: Long) = this.plusDays(daysToAdd)
